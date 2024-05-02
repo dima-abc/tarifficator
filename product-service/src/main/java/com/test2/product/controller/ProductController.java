f402ddc6434e116f980afa413e7e6e1ae84da6ad
dima_abc <haoos@inbox.ru>
2024-05-02 10:30:46 +0300

    Create controller

--- product-service/src/main/java/com/test2/product/controller/ProductController.java
+++ product-service/src/main/java/com/test2/product/controller/ProductController.java
@@ -3,18 +3,15 @@ package com.test2.product.controller;
 import com.test2.product.payload.ProductDTO;
 import com.test2.product.payload.UpdateProduct;
 import com.test2.product.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("api/v1/products/{uuid}")
@@ -26,7 +23,7 @@ public class ProductController {
     @ModelAttribute("product")
     public ProductDTO getProduct(@PathVariable("uuid") String uuid) {
         return this.productService.findProductById(uuid)
-                .orElseThrow(() -> new NoSuchElementException("product_service.error.product.not_found"));
+                .orElseThrow(() -> new NoSuchElementException("product_service.errors.not.found"));
     }
 
     @GetMapping
@@ -36,7 +33,7 @@ public class ProductController {
 
     @PatchMapping
     public ResponseEntity<?> updateProduct(@PathVariable("uuid") String uuid,
-                                           @RequestBody UpdateProduct updateProduct,
+                                           @Valid @RequestBody UpdateProduct updateProduct,
                                            BindingResult bindingResult) throws BindException {
         if (bindingResult.hasErrors()) {
             if (bindingResult instanceof BindException exception) {
@@ -56,14 +53,5 @@ public class ProductController {
         this.productService.deleteProductById(uuid);
         return ResponseEntity.noContent()
                 .build();
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ProblemDetail> handleNoSuchElementException(NoSuchElementException exception,
-                                                                      Locale locale) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
-                        Objects.requireNonNull(this.messageSource.getMessage(exception.getMessage(), new Object[0],
-                                exception.getMessage(), locale))));
     }
 }
